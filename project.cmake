8b8b49a3ce2b911afbdc766f5ffe214c8dca15c5
darkessence87 <darkessence87@gmail.com>
2023-12-13 22:35:51 +0100

    cmake re-structure

---
+++ project.cmake
@@ -0,0 +1,61 @@
+# project name
+message ("Working dir: ${CMAKE_CURRENT_SOURCE_DIR}")
+get_filename_component (projectName ${CMAKE_CURRENT_SOURCE_DIR} NAME)
+string (REPLACE " " "_" projectName ${projectName})
+message ("Project: ${projectName}")
+
+# default build type
+if (NOT EXISTS ${CMAKE_BUILD_TYPE})
+    set (CMAKE_BUILD_TYPE Debug)
+endif()
+
+# create build folder
+set (BUILD_DIR ${CMAKE_CURRENT_SOURCE_DIR}/build)
+STRING (REGEX REPLACE "\\\\" "/" BUILD_DIR ${BUILD_DIR})
+file (MAKE_DIRECTORY ${BUILD_DIR}/bin/)
+message ("Build dir: ${BUILD_DIR}")
+
+# create output folder
+set (BUILD_OUT ${BUILD_DIR}/bin/${CMAKE_BUILD_TYPE})
+set (CMAKE_RUNTIME_OUTPUT_DIRECTORY_DEBUG ${BUILD_OUT})
+set (CMAKE_RUNTIME_OUTPUT_DIRECTORY_RELEASE ${BUILD_OUT})
+set (CMAKE_LIBRARY_OUTPUT_DIRECTORY_DEBUG ${BUILD_OUT})
+set (CMAKE_LIBRARY_OUTPUT_DIRECTORY_RELEASE ${BUILD_OUT})
+set (CMAKE_ARCHIVE_OUTPUT_DIRECTORY_DEBUG ${BUILD_OUT})
+set (CMAKE_ARCHIVE_OUTPUT_DIRECTORY_RELEASE ${BUILD_OUT})
+file (MAKE_DIRECTORY ${BUILD_OUT})
+message ("Build out: ${BUILD_OUT}")
+
+# functions
+function(psi_make_tests name src libs)
+    if (WIN32)
+        set (PLATFORM_LIBS "dbghelp.dll" "ntdll.dll")
+    endif()
+
+    include_directories (${3rdPARTY_DIR}/tests/include)
+    link_directories (${3rdPARTY_DIR}/tests/lib/${SUB_DIR_LIBS})
+    
+    set (fileName PSI_TEST_${name})
+    add_executable (${fileName} ${PROJECT_SOURCE_DIR}/tests/EntryPoint.cpp ${src})
+    target_link_libraries (${fileName}
+        ${libs}
+        ${PLATFORM_LIBS}
+        gmock
+        gmock_main
+        gtest
+        gtest_main
+    )
+endfunction()
+
+function(psi_make_examples name src libs)
+    if (WIN32)
+        set (PLATFORM_LIBS "dbghelp.dll" "ntdll.dll")
+    endif()
+
+    set (fileName PSI_EXAMPLE_${name})
+    add_executable (${fileName} ${src})
+    target_link_libraries (${fileName}
+        ${libs}
+        ${PLATFORM_LIBS}
+    )
+endfunction()
