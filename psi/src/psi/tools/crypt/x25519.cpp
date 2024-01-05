b9482877061880b4fbaf87a0ee0610bbbb2e9d41
darkessence87 <darkessence87@gmail.com>
2024-01-05 12:20:14 +0100

    x25519 cleanups

--- psi/src/psi/tools/crypt/x25519.cpp
+++ psi/src/psi/tools/crypt/x25519.cpp
@@ -9,53 +9,57 @@ const x25519::field_elem x25519::_121665 = {0xdb41, 1};
 
 void x25519::unpack25519(field_elem out, const uint8_t *in)
 {
-    int i;
-    for (i = 0; i < 16; ++i)
+    for (uint8_t i = 0; i < 16; ++i) {
         out[i] = in[2 * i] + ((int64_t)in[2 * i + 1] << 8);
+    }
     out[15] &= 0x7fff;
 }
 
 void x25519::carry25519(field_elem elem)
 {
-    int i;
-    int64_t carry;
-    for (i = 0; i < 16; ++i) {
+    int64_t carry = 0;
+    for (uint8_t i = 0; i < 16; ++i) {
         carry = elem[i] >> 16;
         elem[i] -= carry << 16;
-        if (i < 15)
+        if (i < 15) {
             elem[i + 1] += carry;
-        else
+        } else {
             elem[0] += 38 * carry;
+        }
     }
 }
 
 void x25519::fadd(field_elem out, const field_elem a, const field_elem b) /* out = a + b */
 {
-    int i;
-    for (i = 0; i < 16; ++i)
+    for (uint8_t i = 0; i < 16; ++i) {
         out[i] = a[i] + b[i];
+    }
 }
 
 void x25519::fsub(field_elem out, const field_elem a, const field_elem b) /* out = a - b */
 {
-    int i;
-    for (i = 0; i < 16; ++i)
+    for (uint8_t i = 0; i < 16; ++i) {
         out[i] = a[i] - b[i];
+    }
 }
 
 void x25519::fmul(field_elem out, const field_elem a, const field_elem b) /* out = a * b */
 {
-    int64_t i, j, product[31];
-    for (i = 0; i < 31; ++i)
+    int64_t product[31];
+    for (uint8_t i = 0; i < 31; ++i) {
         product[i] = 0;
-    for (i = 0; i < 16; ++i) {
-        for (j = 0; j < 16; ++j)
-            product[i + j] += a[i] * b[j];
     }
-    for (i = 0; i < 15; ++i)
+    for (uint8_t i = 0; i < 16; ++i) {
+        for (uint8_t j = 0; j < 16; ++j) {
+            product[i + j] += a[i] * b[j];
+        }
+    }
+    for (uint8_t i = 0; i < 15; ++i) {
         product[i] += 38 * product[i + 16];
-    for (i = 0; i < 16; ++i)
+    }
+    for (uint8_t i = 0; i < 16; ++i) {
         out[i] = product[i];
+    }
     carry25519(out);
     carry25519(out);
 }
@@ -63,22 +67,25 @@ void x25519::fmul(field_elem out, const field_elem a, const field_elem b) /* out
 void x25519::finverse(field_elem out, const field_elem in)
 {
     field_elem c;
-    int i;
-    for (i = 0; i < 16; ++i)
+    for (uint8_t i = 0; i < 16; ++i) {
         c[i] = in[i];
-    for (i = 253; i >= 0; i--) {
-        fmul(c, c, c);
-        if (i != 2 && i != 4)
-            fmul(c, c, in);
     }
-    for (i = 0; i < 16; ++i)
+    for (int16_t i = 253; i >= 0; i--) {
+        fmul(c, c, c);
+        if (i != 2 && i != 4) {
+            fmul(c, c, in);
+        }
+    }
+    for (uint8_t i = 0; i < 16; ++i) {
         out[i] = c[i];
+    }
 }
 
 void x25519::swap25519(field_elem p, field_elem q, int64_t bit)
 {
-    int64_t t, i, c = ~(bit - 1);
-    for (i = 0; i < 16; ++i) {
+    const int64_t c = ~(bit - 1);
+    int64_t t;
+    for (uint8_t i = 0; i < 16; ++i) {
         t = c & (p[i] ^ q[i]);
         p[i] ^= t;
         q[i] ^= t;
@@ -87,16 +94,17 @@ void x25519::swap25519(field_elem p, field_elem q, int64_t bit)
 
 void x25519::pack25519(uint8_t *out, const field_elem in)
 {
-    int i, j, carry;
+    int carry;
     field_elem m, t;
-    for (i = 0; i < 16; ++i)
+    for (uint8_t i = 0; i < 16; ++i) {
         t[i] = in[i];
+    }
     carry25519(t);
     carry25519(t);
     carry25519(t);
-    for (j = 0; j < 2; ++j) {
+    for (uint8_t j = 0; j < 2; ++j) {
         m[0] = t[0] - 0xffed;
-        for (i = 1; i < 15; i++) {
+        for (uint8_t i = 1; i < 15; i++) {
             m[i] = t[i] - 0xffff - ((m[i - 1] >> 16) & 1);
             m[i - 1] &= 0xffff;
         }
@@ -105,7 +113,7 @@ void x25519::pack25519(uint8_t *out, const field_elem in)
         m[14] &= 0xffff;
         swap25519(t, m, 1 - carry);
     }
-    for (i = 0; i < 16; ++i) {
+    for (uint8_t i = 0; i < 16; ++i) {
         out[2 * i] = t[i] & 0xff;
         out[2 * i + 1] = static_cast<uint8_t>(t[i] >> 8);
     }
@@ -118,26 +126,28 @@ void x25519::scalarmult_base(uint8_t *out, const uint8_t *scalar)
 
 void x25519::generate_keypair(uint8_t *pk, uint8_t *sk)
 {
-    memcpy(sk, Encryptor::generateSessionKey().data(), 32);
+    memcpy(sk, Encryptor::generateSessionKey().data(), 32u);
     scalarmult_base(pk, sk);
 }
 
 void x25519::scalarmult(uint8_t *out, const uint8_t *scalar, const uint8_t *point)
 {
     uint8_t clamped[32];
-    int64_t bit, i;
     field_elem a, b, c, d, e, f, x;
-    for (i = 0; i < 32; ++i)
+    for (uint8_t i = 0; i < 32; ++i) {
         clamped[i] = scalar[i];
+    }
     clamped[0] &= 0xf8;
     clamped[31] = (clamped[31] & 0x7f) | 0x40;
     unpack25519(x, point);
-    for (i = 0; i < 16; ++i) {
+    for (uint8_t i = 0; i < 16; ++i) {
         b[i] = x[i];
         d[i] = a[i] = c[i] = 0;
     }
     a[0] = d[0] = 1;
-    for (i = 254; i >= 0; --i) {
+
+    int64_t bit;
+    for (int16_t i = 254; i >= 0; --i) {
         bit = (clamped[i >> 3] >> (i & 7)) & 1;
         swap25519(a, b, bit);
         swap25519(c, d, bit);
